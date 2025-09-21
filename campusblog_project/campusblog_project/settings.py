4308c4e6199c64dec3bd4ae87babd2a2f830345e
JhonLauro <jhongil.lauro@cit.edu>
2025-09-22 01:11:32 +0800

    your commit message here

--- campusblog_project/campusblog_project/settings.py
+++ campusblog_project/campusblog_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'campusblog_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'campusblog_project' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,8 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'static_root'
+STATICFILES_DIRS = [BASE_DIR / 'campusblog_project' / 'static']
+STATIC_ROOT = BASE_DIR / 'campusblog_project' / 'static_root'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
