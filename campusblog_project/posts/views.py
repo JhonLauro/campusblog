733b19c69ef33c42b989754cccaa718c4b424e89
JhonLauro <jhongil.lauro@cit.edu>
2025-09-22 01:09:59 +0800

    your commit message here

--- campusblog_project/posts/views.py
+++ campusblog_project/posts/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-# Create your views here.
 def home(request):
     posts = Post.objects.order_by('-created_at')
-    return render(request, 'posts/home.html', {'posts': posts})
+    return render(request, 'home.html', {'posts': posts})
